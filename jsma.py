8b3f6a107c581231c7999e5889f4b2babf91cfcc
cty <cty002718@gmail.com>
2017-11-21 23:43:55 +0800

    jsma modify1

--- jsma.py
+++ jsma.py
@@ -9,8 +9,7 @@ from Structure import cnn_structure, fc_structure, sub_structure
 from FCpredict import compute_accuracy, predict_from_array, predict_from_dir_to_dir
 import matplotlib.pyplot as plt
 
-
-def jsmaOnePicture(sess, image, xs, keep_prob, correct_predict, target, theta, grads):
+def jsmaOnePicture(predict, num, a, sess, image, xs, keep_prob, correct_predict, target, theta, grads):
     adv_x = np.copy(image)
 
     if theta > 0:
@@ -20,6 +19,8 @@ def jsmaOnePicture(sess, image, xs, keep_prob, correct_predict, target, theta, g
         search_domain = set([i for i in range(784)
                              if adv_x[0, i] > 0])
 
+    add_x = np.zeros((1, 784), dtype=float)
+
     iteration = 0
     current = sess.run(correct_predict, feed_dict={xs: adv_x, keep_prob: 1})
 
@@ -28,6 +29,7 @@ def jsmaOnePicture(sess, image, xs, keep_prob, correct_predict, target, theta, g
 
         grads_target = np.zeros((1, 784), dtype=float)
         grads_other = np.zeros((1, 784), dtype=float)
+        side_add = np.zeros((1, 784), dtype=float)
 
         for i, grad in enumerate(grads):
             run_grad = sess.run(grad, feed_dict={xs: adv_x, keep_prob: 1})
@@ -37,39 +39,53 @@ def jsmaOnePicture(sess, image, xs, keep_prob, correct_predict, target, theta, g
 
             grads_other += np.reshape(run_grad, (1, 784))
 
+        for i in range(784):
+            sum = 0
+            p1, p2 = i // 28, i % 28
+            tmp1l, tmp1r = p1+1, p2
+            tmp2l, tmp2r = p1-1, p2
+            tmp3l, tmp3r = p1, p2+1
+            tmp4l, tmp4r = p1, p2-1
+            if tmp1l < 28:
+                sum += adv_x[0][tmp1l*28+tmp1r]
+            if tmp2l >= 0:
+                sum += adv_x[0][tmp2l*28+tmp2r]
+            if tmp3r < 28:
+                sum += adv_x[0][tmp3l*28+tmp3r]
+            if tmp4r >= 0:
+                sum += adv_x[0][tmp4l*28+tmp4r]
+
+            if sum >= 1:
+                side_add[0][i] = 1
+
         invalid = list(set(range(784)) - search_domain)
         grads_target[0][invalid] = -theta * np.max(np.abs(grads_target))
         grads_other[0][invalid] = theta * np.max(np.abs(grads_other))
 
-        target_sum = grads_target.reshape((1, 784)) + grads_target.reshape((784, 1))
-        other_sum = grads_other.reshape((1, 784)) + grads_other.reshape((784, 1))
-
         if theta > 0:
-            scores_mask = ((target_sum > 0) & (other_sum < 0))
+            scores_mask = ((grads_target > 0) & (grads_other < 0))
         else:
-            scores_mask = ((target_sum < 0) & (other_sum > 0))
+            scores_mask = ((grads_target < 0) & (grads_other > 0))
 
-        scores = scores_mask * np.abs(target_sum * other_sum)
+        for i in range(784):
+            if scores_mask[0][i]:
+                if side_add[0][i] == 1:
+                    if theta > 0:
+                        adv_x[0, i] = np.minimum(1, adv_x[0, i] + theta)
+                        add_x[0, i] += theta
+                    else:
+                        adv_x[0, i] = np.maximum(0, adv_x[0, i] - theta)
+                        add_x[0, i] += theta
 
-        #np.fill_diagonal(scores, -1000)
+        for i in range(784):
+            if adv_x[0][i] == 1:
+                search_domain.discard(i)
 
-        best = np.argmax(scores)
-        if best == 0:
-            break
-        p1, p2 = best % 784, best // 784
-
-        search_domain.discard(p1)
-        search_domain.discard(p2)
-
-        if theta > 0:
-            adv_x[0, p1] = np.minimum(1, adv_x[0, p1] + theta)
-            adv_x[0, p2] = np.minimum(1, adv_x[0, p2] + theta)
-        else:
-            adv_x[0, p1] = np.minimum(0, adv_x[0, p1] - theta)
-            adv_x[0, p2] = np.minimum(0, adv_x[0, p2] - theta)
-
+        #print(sess.run(predict, feed_dict={xs: adv_x, keep_prob: 1}))
         current = sess.run(correct_predict, feed_dict={xs: adv_x, keep_prob: 1})
         iteration = iteration + 1
+
+
 
     return current, adv_x
 
@@ -82,11 +98,17 @@ def jsma():
     with g_sub_make.as_default():
         xs = tf.placeholder(tf.float32, [None, 784])
         keep_prob = tf.placeholder(tf.float32)
-        predict, correct_predict = cnn_structure(xs, keep_prob)
+        if DIR == 'ADV_FC':
+            predict, correct_predict = fc_structure(xs, keep_prob)
+        if DIR == 'ADV_CNN':
+            predict, correct_predict = cnn_structure(xs, keep_prob)
         saver = tf.train.Saver()
 
     sess = tf.Session(graph=g_sub_make)
-    saver.restore(sess, "CNN_net/save_net.ckpt")
+    if DIR == 'ADV_FC':
+        saver.restore(sess, "FC_net/save_net.ckpt")
+    if DIR == 'ADV_CNN':
+        saver.restore(sess, "CNN_net/save_net.ckpt")
 
     gradients = []
     for i in range(10):
@@ -101,12 +123,21 @@ def jsma():
 
     count = 0
 
+    f, a = plt.subplots(10, 11, figsize=(12, 4))
+
+    suc = 0
+    reali = 0
     for target in range(10):
+        suc_pic = 0
         for i, image in enumerate(MNIST):
             image = np.reshape(image, (1, 784))
             test1 = sess.run(correct_predict, feed_dict={xs: image, keep_prob: 1})
-            test2, adv_x = jsmaOnePicture(sess, image, xs, keep_prob, correct_predict, target, THETA, gradients)
+            if test1 == target:
+                continue
 
+            test2, adv_x = jsmaOnePicture(predict, reali, a, sess, image, xs, keep_prob, correct_predict, target, THETA, gradients)
+            reali += 1
+            '''
             if target == test2:
                 suc = 1
             else:
@@ -117,9 +148,19 @@ def jsma():
             im = Image.fromarray(im)
             im.save('%s/%s/%s_%s.bmp' % (DIR, str(target), str(count), str(suc)), 'bmp')
             count = count + 1
+            '''
+            if target == test2:
+                suc += 1
+                if suc_pic < 10:
+                    a[target][suc_pic].imshow(np.reshape(adv_x, (28, 28)), plt.cm.gray)
+                    a[target][suc_pic].set_xticks([])
+                    a[target][suc_pic].set_yticks([])
+                    suc_pic += 1
 
             print(i, test1, test2)
 
+    print(suc / reali)
+    plt.show()
 
     #for i, d in enumerate(deltax):
     #    mean = np.mean(d)
